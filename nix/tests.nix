336e6866b7c95f63f5352508651960eee64da267
Alan Strohm <astrohm@google.com>
2026-03-26 21:32:09 -0700

    Fix declare-binscript with imports

--- nix/tests.nix
+++ nix/tests.nix
@@ -1,5 +1,4 @@
-pkgs:
-{
+pkgs: {
   binscript-bin =
     let
       src = pkgs.symlinkJoin {
@@ -24,6 +23,38 @@ pkgs:
     pkgs.runCommand "binscript-bin" { } ''
       output=$(${pkg}/bin/test)
       [ "$output" = "binscript-bin" ] || (echo "Unexpected output: $output"; exit 1)
+      touch $out
+    '';
+
+  binscript-import =
+    let
+      src = pkgs.symlinkJoin {
+        name = "src";
+        paths = [
+          (pkgs.writeTextDir "project.janet" ''
+            (declare-project :name "myproj")
+            (declare-source :source [ "src/helper.janet" ])
+            (declare-binscript :main "src/test")
+          '')
+          (pkgs.writeTextDir "src/helper.janet" ''
+            (defn greeting [] "binscript-import")
+          '')
+          (pkgs.writeTextDir "src/test" ''
+            #!/usr/bin/env janet
+            (import helper)
+            (defn main [& args] (print (helper/greeting)))
+          '')
+        ];
+      };
+      pkg = pkgs.mkJanet {
+        inherit src;
+        name = "test";
+        bin = "test";
+      };
+    in
+    pkgs.runCommand "binscript-import" { } ''
+      output=$(${pkg}/bin/test)
+      [ "$output" = "binscript-import" ] || (echo "Unexpected output: $output"; exit 1)
       touch $out
     '';
 }
